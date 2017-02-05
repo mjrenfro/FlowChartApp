2d85290ef7a6c7bf049b590fa3c08aa824ef30f9
mjrenfro <mrenfro01@gmail.com>
2017-02-04 17:03:37 -0700

    Ensured collection actually is deleted with callback

--- index.js
+++ index.js
@@ -19,37 +19,22 @@ var url='mongodb://localhost:27017/test';
 const crypto=require('crypto');
 
 
-var insertString= function(db,res,hash_string, callback){
-  // const coll=db.get(current_session.collection);
-  // console.log(coll.find());
-  // coll.insert({
-  //   "key": current_session.key,
-  //   "word":current_session.value,
-  //   "hashed":hash_string
-  // });
-
+function insertString(db,res,hash_string, callback){
   MongoClient.connect(url, function(err, db){
     assert.equal(null, err);
     db.collection(current_session.collection).insertOne({key:current_session.key,
-      word:current_session.value, hashed: hash_string}, function (err, r){
+      word:current_session.value, json: {key:current_session.key, word:current_session.value}}, function (err, r){
 
         assert.equal(null, err);
         assert.equal(1, r.insertedCount);
-          all_documents(db, res, callback);
+          all_documents(db, {},res);
         db.close();
       });
-
-
   });
     callback();
 };
 
-
-
-//TODO: error checking for when key is not found
-
-var searchString=function(db,return_val, res,callback){
-  console.log("Collection name: ", current_session.collection);
+function searchString(db,return_val, res,callback){
   const coll=db.get(current_session.collection);
   coll.find({
     "key":current_session.key
@@ -57,27 +42,21 @@ var searchString=function(db,return_val, res,callback){
       var found_value=stuff_found[0]["word"]
       current_session.value=found_value;
       bundle={previous:found_value};
-
-      //have to update the display
-      get_all_documents(db, bundle, res);
-      // console.log("-------------------");
-      // console.log(bundle);
-      // console.log("-------------------");
-      // html_render(bundle, res);
+      all_documents(db, bundle, res);
   });
   callback();
 };
 
-//TODO: is callback really needed?
 var updateString=function(db, new_string, res, callback){
   const coll=db.get(current_session.collection);
   coll.update({key:current_session.key},{$set:{word:new_string}}).then(()=>{
     current_session.value=new_string;
-  all_documents(db, res, callback);
+  all_documents(db, {},res);
   });
+  callback();
 }
 
-function get_dbs(res, callback){
+function get_dbs(res){
   name_bds=[];
 
   MongoClient.connect(url, function(err, db){
@@ -86,38 +65,28 @@ function get_dbs(res, callback){
     adminDb.listDatabases(function(err, dbs){
       assert.equal(null, err);
       assert.ok(dbs.databases.length>0);
-      for (d of dbs.databases){
-        console.log(d['name']);
-      }
       db.close();
     });
   });
-
-  callback();
-
 }
 
 //should return only names or the entire obj
-function get_collections(res,callback){
+function get_collections(res){
   name_colls=[];
 
   MongoClient.connect(url,function(err, db){
     assert.equal(null, err);
     db.listCollections().toArray(function(err,collInfos){
       for (let coll of collInfos){
-        console.log("adding names");
-        name_colls.push(coll['name']);
+          name_colls.push(coll['name']);
       }
+            db.close()
             res.render('main', {collections:name_colls});
     });
 
   });
-  callback();
 }
-function render_docs (res, docs){
-  console.log(docs);
-  html_render({}, res);
-}
+
 //Could  not find a direct way to create a collection in the MongoDB driver
 function createCollection(collection){
   collection.insertOne({a:1}, function(err, r){
@@ -128,40 +97,18 @@ function createCollection(collection){
     assert.equal(null, err);
     assert.equal(1,r.deletedCount);
   });
-  // html_render({}, res);
 }
-//docs, bundle, res
-function get_parse_docs(docs, bundle, res){
 
-  console.log("docs: ", docs);
-
-
+function parse_docs(docs, bundle, res){
   docs_bundle=[];
   for (let d of docs){
-    // console.log(d);
-    docs_bundle.push([d['key'], d['word']]);
+      docs_bundle.push([d['key'], d['word']]);
   }
-
   bundle['documents']=docs_bundle;
-  console.log("########################");
-  console.log(bundle);
-  console.log("########################");
-  html_render(bundle, res);
-
-}
-function parse_docs(docs, res){
-  docs_bundle=[];
-  for (let d of docs){
-    console.log(d);
-    docs_bundle.push([d['key'], d['word']]);
-  }
-  html_render({documents:docs_bundle}, res);
+  res.render('crud',bundle);
 }
 
 function deleteDocument(db, res,callback){
-
-  // const coll = db.get(current_session.collection);
-  // coll.remove({key:key_value});
 
   MongoClient.connect(url, function(err, db){
     assert.equal(null, err);
@@ -172,23 +119,36 @@ function deleteDocument(db, res,callback){
     });
 
   });
-    all_documents(db, res, callback);
+    all_documents(db,{}, res);
 
 }
 
 function delete_collection(res, req){
   MongoClient.connect(url, function(err, db){
     assert.equal(null, err);
-    db.collection(current_session.collection).drop();
-    current_session.collection=null;
+    console.log("CS: ",current_session.collection);
 
-    get_dbs(res, function(){
-      console.log("end of function");
+    //gotta love callbacks
+    db.collection(current_session.collection).drop(function (err, r){
+        //Error checking code
+        db.listCollections().toArray(function(err, r){
+          var found = false;
+          r.forEach(function(document){
+            //es no bueno
+            if(document.name==current_session.collection){
+              found=true;
+              return;
+            }
+          });
+          assert.equal(false, found);
 
-    });
-    list_names=get_collections(res, function(){
-      console.log("called automatically");
-      req.db.close();
+          db.close();
+        });
+        current_session.collection=null;
+        get_dbs(res);
+        list_names=get_collections(res);
+
+
     });
 
 
@@ -200,70 +160,41 @@ function delete_collection(res, req){
   Query the current "session" (still need to implement sessions :( )
   and the update the contents panel, obviously with the contents of the collection
  */
-
- function get_all_documents(db, bundle, res){
-
-   MongoClient.connect(url, function(err, db){
-     assert.equal(null, err);
-     console.log("connected to the server");
-     const coll=db.collection(current_session.collection);
-     coll.find().toArray(function(err, docs){
-         assert.equal(null, err);
-         console.log("no errors with find...but are there docs???");
-         //new collection needs to be made
-         if (docs.length ==0){
-           createCollection(coll);
-
-         }
-        get_parse_docs(docs, bundle, res);
-     });
-   });
-
- }
-
-function all_documents(db, res, callback){
+function all_documents(db,bundle, res){
 
   MongoClient.connect(url, function(err, db){
     assert.equal(null, err);
-    console.log("connected to the server");
     const coll=db.collection(current_session.collection);
     coll.find().toArray(function(err, docs){
         assert.equal(null, err);
-        console.log("no errors with find...but are there docs???");
         //new collection needs to be made
         if (docs.length ==0){
           createCollection(coll);
-
         }
-        parse_docs(docs, res)
+        parse_docs(docs, bundle, res)
     });
   });
 
-  callback();
 }
-var html_render=function(bundle, res){
-  res.render('crud',bundle);
-}
-
 router.get('/', function(req, res){
-    get_dbs(res, function(){
-      console.log("end of function");
-
-    });
-    list_names=get_collections(res, function(){
-      console.log("called automatically");
-      req.db.close();
-    });
+    get_dbs(res);
+    list_names=get_collections(res);
 });
 
 router.post('/', function(req, res){
-  console.log("post");
+  console.log("delete_coll button: ", req.body.delete_coll);
+  if (req.body.delete_coll!=undefined){
+
+    console.log("in delete coll");
+    delete_collection(res, req);
+  }
+  else{
   current_session.collection = typeof req.body.name!="undefined" ? req.body.name :req.body.which_coll;
-  console.log(current_session.collection);
-  all_documents(req.db, res, function(){
-    req.db.close();
-  });
+  console.log("else CS ", current_session.collection);
+  all_documents(req.db, {},res);
+}
 });
+
 router.post('/update', function(req,res){
   var new_word=req.body.found;
   updateString(req.db, new_word, res, function(){
@@ -277,9 +208,7 @@ router.post('/delete', function(req, res){
     req.db.close();
   });
 });
-router.post('/delete_collection', function(req, res){
-  delete_collection(res, req);
-});
+
 router.post('/enter', function (req, res){
   current_session.key = req.body.key;
   current_session.value= req.body.value;
@@ -289,9 +218,6 @@ router.post('/enter', function (req, res){
   insertString(req.db,res,hash, function(){
     req.db.close();
   });
-
-  //Wrapping up value for mustache and sending it back
-  // html_render({hashed:hash}, res );
 
 });
 
